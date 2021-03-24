cdd8808ec5927815afe387fca5a41115a820bc6a
Ben Mohammad <benmohammad593@gmail.com>
2021-03-24 17:13:40 +0000

    HomeFragment.kt added complete...

--- app/src/main/java/com/benmohammad/nynuze/ui/home/HomeFragment.kt
+++ app/src/main/java/com/benmohammad/nynuze/ui/home/HomeFragment.kt
@@ -1,6 +1,119 @@
 package com.benmohammad.nynuze.ui.home
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.benmohammad.nynuze.HOME_NEWS
+import com.benmohammad.nynuze.NyApp
+import com.benmohammad.nynuze.R
+import com.benmohammad.nynuze.dagger.modules.ViewModelFactory
+import com.benmohammad.nynuze.ui.adapter.NewsAdapter
+import com.benmohammad.nynuze.viewState.NewsViewEvent
+import com.benmohammad.nynuze.viewState.NewsViewResult
+import com.benmohammad.nynuze.viewState.NewsViewState
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.subjects.PublishSubject
+import kotlinx.android.synthetic.main.fragment_home.*
+import javax.inject.Inject
+import javax.security.auth.login.LoginException
 
 class HomeFragment: Fragment() {
+
+    @Inject lateinit var viewModelFactory: ViewModelFactory
+    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var uiDisposable: Disposable
+    private lateinit var disposable: Disposable
+    private val newsAdapter: NewsAdapter by lazy {NewsAdapter(requireContext(), HOME_NEWS)}
+    private val swipeRefresh: PublishSubject<NewsViewEvent.ScreenLoadEvent>  = PublishSubject.create()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        NyApp.getApp(requireContext()).mainComponent.injectHome(this)
+        super.onCreate(savedInstanceState)
+        configureViewModel()
+        observeViewState()
+    }
+
+    private fun configureViewModel() {
+        homeViewModel = ViewModelProvider(requireActivity(), viewModelFactory).get(HomeViewModel::class.java)
+    }
+
+    private fun observeViewState() {
+        disposable = homeViewModel.viewState
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                renderViewState(it)
+            }, {
+                Log.e("HomeFragment", "Error with viewState")
+            })
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        insetWindow()
+        init()
+    }
+
+    private fun insetWindow() {
+        cl.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        cl.setOnApplyWindowInsetsListener{v, insets ->
+            rv_home.setPadding(0, insets.systemWindowInsetTop,0,0)
+            cl.setOnApplyWindowInsetsListener(null)
+            insets.consumeSystemWindowInsets()
+        }
+    }
+
+    private fun init() {
+        rv_home.adapter = newsAdapter
+        swipe_refresh_home.setOnRefreshListener(onRefreshListener)
+        val screenLoadEvent: Observable<NewsViewEvent.ScreenLoadEvent> = swipeRefresh
+        uiDisposable = screenLoadEvent.subscribe({
+            homeViewModel.processInput(it)
+        }, {
+            Log.d("HomeFragment", "$it error processing")
+        })
+
+        swipeRefresh.onNext(NewsViewEvent.ScreenLoadEvent)
+    }
+
+    private fun renderViewState(it: NewsViewState?) {
+        swipe_refresh_home.isRefreshing = it?.isLoading ?: false
+        if(it?.isEmpty == true) {
+            rv_home.visibility = View.INVISIBLE
+            empty_view.visibility = View.VISIBLE
+        } else {
+            empty_view.visibility = View.GONE
+            rv_home.visibility = View.VISIBLE
+            newsAdapter.submitList(it?.adapterList)
+        }
+        it?.error.let {
+            if(!it.isNullOrEmpty()) Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private val onRefreshListener = SwipeRefreshLayout.OnRefreshListener {
+        swipeRefresh.onNext(NewsViewEvent.ScreenLoadEvent)
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        uiDisposable.dispose()
+    }
 }
